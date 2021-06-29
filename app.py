42a84b770587c3a5ee7ef786f55362b143a9e381
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-06-29 22:20:14 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home , VeriGorsellestirme
+from apps import home , VeriAnaliziFinal
 
 app = MultiApp()
 
+st.image('./fibabanka-logo.png')
 st.markdown("UYGULAMALAR")
 
-# Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Veri Görselleştirme", VeriGorsellestirme.app)
-# The main app
+app.add_app("Veri Görselleştirme", VeriAnaliziFinal.app)
+
 app.run()
