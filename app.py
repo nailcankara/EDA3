047f6fb76b8f76e0a09aa0d8619f47691a438215
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-06-29 22:21:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home , VeriAnaliziFinal
+from apps import home , VeriGorsellestirme
 
 app = MultiApp()
 
@@ -8,6 +8,6 @@ st.image('./fibabanka-logo.png')
 st.markdown("UYGULAMALAR")
 
 app.add_app("Home", home.app)
-app.add_app("Veri Görselleştirme", VeriAnaliziFinal.app)
+app.add_app("Veri Görselleştirme", VeriGorsellestirme.app)
 
 app.run()
